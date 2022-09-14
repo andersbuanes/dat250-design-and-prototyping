eda90dcb4b7683795789d139e4af3bbeb864a324
Rune Mæstad <rune.maestad@gmail.com>
2022-09-14 11:32:03 +0200

    Implemented poll entity

--- src/main/java/models/Poll.java
+++ src/main/java/models/Poll.java
@@ -1,4 +1,35 @@
 package models;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
 public class Poll {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    public UUID id;
+
+    public int pincode;
+    public String question;
+    public String answerA;
+    public String answerB;
+    public boolean isPrivate;
+    public LocalDateTime stateDate;
+    public LocalDateTime endDate;
+    public boolean isClosed;
+
+    @ManyToOne
+    public User user;
+
+    @OneToMany(mappedBy = "poll")
+    public List<Vote> votes;
+
+    @OneToMany(mappedBy = "poll")
+    public List<IoTVotes> iotVotes;
+
 }
