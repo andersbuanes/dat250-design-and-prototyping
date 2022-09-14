9e890e1d6987c981853892ab31227fb90c689659
Erlend.Skutlaberg <erlend.skutlaberg@student.uib.no>
2022-09-14 11:35:42 +0200

    Implemented User and Vote

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,27 @@
 package models;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Entity
+@Data
 public class User {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    private UUID id;
+
+    private String name;
+    private boolean isAdmin;
+
+    @OneToMany(mappedBy = "user")
+    private ArrayList<Vote> votes;
+
+
+
+
+
+
 }
--- src/main/java/models/Vote.java
+++ src/main/java/models/Vote.java
@@ -1,4 +1,35 @@
 package models;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
 public class Vote {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    public UUID id;
+
+    //Can be implemented further later
+    enum Answer {
+        Yes, NO
+    }
+
+    public Answer answer;
+
+
+    @OneToMany
+    public Set<Poll> polls;
+
+    @OneToMany
+    public Set<User> user;
+
+
+
+
+
+
 }
