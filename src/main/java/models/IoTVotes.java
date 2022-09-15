a91920412e3d38e496e591172d00a6d2ea6d60ce
Erlend.Skutlaberg <erlend.skutlaberg@student.uib.no>
2022-09-15 11:21:41 +0200

    Implemented IoTVotingDevice and IoTVotes

--- src/main/java/models/IoTVotes.java
+++ src/main/java/models/IoTVotes.java
@@ -1,4 +1,30 @@
 package models;
 
+import lombok.Data;
+import org.eclipse.persistence.annotations.UuidGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@UuidGenerator(name = "pollIdGenerator")
+@Data
 public class IoTVotes {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    public String id;
+
+    @Id
+    public int pincode;
+
+    public int answerA;
+    public int answerB;
+
+    @ManyToOne
+    public IoTVotingDevice votingDevice;
+
+
 }
--- src/main/java/models/IoTVotingDevice.java
+++ src/main/java/models/IoTVotingDevice.java
@@ -1,4 +1,27 @@
 package models;
 
+import lombok.Data;
+import org.eclipse.persistence.annotations.UuidGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
+@UuidGenerator(name = "pollIdGenerator")
+@Data
 public class IoTVotingDevice {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    public String id;
+
+    public int pincode;
+
+    public String name;
+
+    @OneToMany(mappedBy = "votingDevice")
+    public Set<IoTVotes> votes;
+
 }
